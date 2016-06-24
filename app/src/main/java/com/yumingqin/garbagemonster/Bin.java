84763eec439b2b1bd107eef9603c444b17389743
Yuming Qin <yumingq@wharton.upenn.edu>
2016-06-24 11:33:36 -0400

    Starting new classes for the game
    new classes for the game!

--- app/src/main/java/com/yumingqin/garbagemonster/Bin.java
+++ app/src/main/java/com/yumingqin/garbagemonster/Bin.java
@@ -4,5 +4,18 @@ package com.yumingqin.garbagemonster;
  * Created by yumin on 6/24/2016.
  */
 public class Bin {
-    String id;
+    String BinID;
+
+    public Bin() {
+        BinID = "Testing";
+    }
+
+    public boolean play(Garbage garbage) {
+        if (garbage.id.equals(BinID)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int score()
 }
