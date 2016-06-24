4128508adf0b343134b425f3230c5f11424730bf
Yuming Qin <yumingq@wharton.upenn.edu>
2016-06-24 11:11:51 -0400

    New Game Activity
    adding a new activity

--- app/src/main/java/com/yumingqin/garbagemonster/MainActivity.java
+++ app/src/main/java/com/yumingqin/garbagemonster/MainActivity.java
@@ -1,12 +1,16 @@
 package com.yumingqin.garbagemonster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+//    String message = "java\com\yumingqin\garbagemonster\MainActivity.java";
+    String message = "com.yumingq.garbagemonster.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View view) {
+        Button startGame = (Button) findViewById(R.id.playGameButton);
         Toast.makeText(getApplicationContext(), "Game Start Testing", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, Game.class);
+        intent.putExtra(message, "Game Start!");
+        startActivity(intent);
     }
 }
